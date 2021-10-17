499b4010ef45a972d2a7a9febf96b11a1c786d9e
vvonderfull <ryazantsev95@inbox.ru>
2021-10-17 13:40:39 +0700

    new

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const chalk = require("chalk");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
-const postRoutes = require("./routes/post-routes");
 const postApiRoutes = require("./routes/api-post-routes");
-const contactRoutes = require("./routes/contact-routes");
-const createPath = require("./helpers/create-path");
 require("dotenv").config();
 
 const errorMsg = chalk.bgKeyword("white").redBright;
@@ -14,11 +11,9 @@ const successMsg = chalk.bgKeyword("green").whiteBright;
 
 const app = express();
 
-app.set("view engine", "ejs");
-
 mongoose
   .connect(process.env.MONGO_URL)
-  .then((res) => {
+  .then(() => {
     console.log(successMsg("Connect DB!"));
   })
   .catch((err) => {
@@ -37,24 +32,10 @@ app.use(
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static("styles"));
-
 app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
-  const title = "Home";
-  res.render(createPath("index"), { title });
-});
-
-app.use(postRoutes);
 app.use(postApiRoutes);
-app.use(contactRoutes);
-
-app.get("/about", (req, res) => {
-  res.redirect("/contacts");
-});
 
 app.use((req, res) => {
-  const title = "ERROR";
-  res.status(404).render(createPath("error"), { title });
+  res.status(404)
 });
