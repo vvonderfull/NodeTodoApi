d9ad4bb15d1eed011b8ef9e715440ef4f3da92d4
vvonderfull <ryazantsev95@inbox.ru>
2021-10-24 13:14:10 +0700

    new

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const chalk = require("chalk");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const cors = require('cors');
 const todoApiRoutes = require("./routes/api-todo-routes");
 const categoryApiRoutes = require("./routes/api-category-routes");
 require("dotenv").config();
@@ -11,6 +12,7 @@ const errorMsg = chalk.bgKeyword("white").redBright;
 const successMsg = chalk.bgKeyword("green").whiteBright;
 
 const app = express();
+app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URL)
